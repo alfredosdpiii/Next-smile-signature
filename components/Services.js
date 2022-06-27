96068a20047e103a8b6ae4c61b8ee406186b6af4
alfredosdpiii <alfredo.sdpiii@gmail.com>
2022-06-28 02:15:12 +0800

    ✨ Added services component

---
+++ components/Services.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Services = () => {
+  return (
+    <div className='w-screen h-screen flex justify-center items-center relative mt-12 animate-[slide_4s_ease-in-out]' id='services'>
+      <h1>Services</h1>
+    </div>
+  )
+}
+
+export default Services
