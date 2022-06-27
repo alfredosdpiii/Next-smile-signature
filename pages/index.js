f34a57ce04e89d8d0946c8c5edf0233b38442248
alfredosdpiii <alfredo.sdpiii@gmail.com>
2022-06-28 02:15:48 +0800

    ✨ Added services

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import Header from "../components/Header";
 import SideNav from "../components/SideNav"
 import Hero from "../components/Hero"
 import About from '../components/About'
+import Services from '../components/Services'
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <SideNav />
       <Hero />
       <About />
+      <Services />
     </Layout>
   )
 }
