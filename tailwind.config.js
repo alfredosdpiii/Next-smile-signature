a591dcd9b22046eb94a4acff931c8cf279019b99
alfredosdpiii <alfredo.sdpiii@gmail.com>
2022-06-28 01:48:56 +0800

    ✨ Added new animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,15 @@ module.exports = {
     extend: {
       fontFamily: {
         'sans': ['DMSans', 'Playfair']
+      },
+      keyframes:{
+        slide: {
+          '0%':{transform: 'translateX(-100%)'},
+          '100%': {transform: 'translateX(0)'},
+        },
+      },
+      animation:{
+        slide: 'slide 1s ease-in-out'
       }
 
     },
