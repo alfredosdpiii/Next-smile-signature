2735cfb30663aa5ca840e85054da85e0809ed1e4
alfredosdpiii <alfredo.sdpiii@gmail.com>
2022-06-29 00:17:38 +0800

    💄 Responsive grid changed to 2 columns

--- components/Services.js
+++ components/Services.js
@@ -12,16 +12,16 @@ import ortho from '../public/orthodonics.jpg'
 export const Card = (props) => {
   return(
     <div>
-      <Image src={props.src} width="2000px" height="1500px" alt="props.alt"  />
-      <h4 className='paragraphs text-center text-3xl font-extralight text-white'>{props.title}</h4> 
+      <Image src={props.src} width="2000px" height="1500px" alt="props.alt" objectFit='contain' layout='responsive'/>
+      <h4 className='paragraphs text-center text-xs sm:text-3xl font-extralight text-white'>{props.title}</h4> 
     </div>
   )
 }
 const Services = () => {
   return (
     <div className='w-screen h-screen flex justify-evenly items-center relative animate-[slide_4s_ease-in-out] flex-col bg-black' id='services'>
-    <h1 className='heading-1 text-7xl text-white'>Services we offer.</h1>
-      <div className='w-8/12 h-8/12 grid grid-cols-4 grid-rows-2 gap-8'>
+    <h1 className='heading-1 text-xl sm:text-7xl text-white'>Services we offer.</h1>
+      <div className='w-8/12 h-8/12 grid grid-cols-2 sm:grid-cols-4 grid-rows-2 gap-8'>
         <Card src={tech} alt={'Picture of x-ray'} title={'X-Ray'}/>
         <Card src={bond} alt={'Picture of bond'} title={'Bonding'}/>
         <Card src={white} alt={'Picture of teeth whitening'} title={'Whitening'}/>
