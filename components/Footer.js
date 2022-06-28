72df7c58142aafbdd5f7fc39676269ebf6b50c67
alfredosdpiii <alfredo.sdpiii@gmail.com>
2022-06-29 01:42:13 +0800

    💄 fixed footer responsiveness

--- components/Footer.js
+++ components/Footer.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='w-screen h-{38} bottom-0 flex flex-row justify-around py-4'>
-    <p className='text-xl'>&copy; 2022</p>
-    <p className='heading-1 text-xl'>Smile Signature</p>
-    <p className='text-xl'>All rights reserved.</p>
+    <div className='w-screen h-{20} sm:h-{38} bottom-0 flex flex-row justify-around py-4'>
+    <p className='text-xs'>&copy; 2022</p>
+    <p className='heading-1 text-xs'>Smile Signature</p>
+    <p className='text-xs'>All rights reserved.</p>
     </div>
   )
 }
