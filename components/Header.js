d8be5431c19b8438bbf10907ad02ea391c95e91b
alfredosdpiii <alfredo.sdpiii@gmail.com>
2022-06-29 01:55:05 +0800

    fixed header responsiveness

--- components/Header.js
+++ components/Header.js
@@ -2,12 +2,12 @@ import React from "react"
 
 const Header = () => {
   return (
-    <nav className="flex w-full bg-transparent justify-between py-4 px-12 sticky top-0 z-10">
-      <a className="heading-1 sm:text-3xl p-4 text-xs">
+    <nav className="flex w-full bg-purple-200 sm:bg-transparent justify-between p-1 sm:py-4 px-12 sticky top-0 z-10">
+      <a className="heading-1 sm:text-3xl p-1 sm:p-4 text-xs">
         Smile Signature
       </a>
       
-      <button className="rounded-full text-green-400 sm:text-black sm:bg-green-200  font-light text-xs p-5 sm:p-4 sm:text-xl">Schedule Appointment</button>
+      <button className="rounded-full text-blue-500 sm:text-black sm:bg-green-200  font-light text-xs p-1 sm:p-4 sm:text-xl">Schedule Appointment</button>
     </nav>
   )
 }
