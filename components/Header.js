c5fcc17ce0bbefa9bd275460d85b88b32a7e486e
alfredosdpiii <alfredo.sdpiii@gmail.com>
2022-06-29 00:02:26 +0800

    💄 ui change font color at small device

--- components/Header.js
+++ components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
         Smile Signature
       </a>
       
-      <button className="rounded-full text-green-200 sm:text-black sm:bg-green-200  font-light text-xs p-5 sm:p-4 sm:text-xl">Schedule Appointment</button>
+      <button className="rounded-full text-green-500 sm:text-black sm:bg-green-200  font-light text-xs p-5 sm:p-4 sm:text-xl">Schedule Appointment</button>
     </nav>
   )
 }
