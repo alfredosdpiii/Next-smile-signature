b1b996aa8b63dcd5f4c45cf3950306faae10797a
alfredosdpiii <alfredo.sdpiii@gmail.com>
2022-06-28 18:43:20 +0800

    💄 Made fonts larger and used thinner font on button

--- components/Header.js
+++ components/Header.js
@@ -3,11 +3,11 @@ import React from "react"
 const Header = () => {
   return (
     <nav className="flex w-full bg-transparent justify-between py-4 px-12 sticky top-0 z-10">
-      <a className="heading-1 text-xl p-4">
+      <a className="heading-1 text-3xl p-4">
         Smile Signature
       </a>
       
-      <button className="rounded-full bg-green-200 p-4 text-xl">Schedule Appointment</button>
+      <button className="rounded-full bg-green-200 p-4 text-xl font-light">Schedule Appointment</button>
     </nav>
   )
 }
--- components/SideNav.js
+++ components/SideNav.js
@@ -3,7 +3,7 @@ import styles from '../styles/SideNav.module.css'
 
 const NavItem = props => (
   <li className="px-2 py-1">
-    <a className="text-lg font-light px-8 py-2 hover:text-blue-500 navigation-links" href={props.href}>
+    <a className="text-2xl font-light px-8 py-2 hover:text-blue-500 navigation-links" href={props.href}>
       {props.text}
     </a>
   </li>
