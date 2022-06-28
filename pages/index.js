3e954aa14e56c7f784cb21e5815b7fd1f82344e4
alfredosdpiii <alfredo.sdpiii@gmail.com>
2022-06-28 16:10:17 +0800

    ✨ added contact component

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import SideNav from "../components/SideNav"
 import Hero from "../components/Hero"
 import About from '../components/About'
 import Services from '../components/Services'
+import Contact from '../components/Contact'
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
         <About className="under" />
       </div>
       <Services className="over"/>
+      <Contact />
     </Layout>
   )
 }
